88c91acf6053bf1bcdb788346c69c9dfb0fb4552
OlegKorniychuk <kornoleg576@gmail.com>
2024-09-25 13:02:01 +0300

    chore: initial commit
    Create project structure

---
+++ back/src/server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: '.env' });
+
+const PORT = process.env.PORT || 3000;
+const dbConnection = process.env.MONGO_DB_CONNECTION.replace(
+  'PASSWORD',
+  process.env.MONGO_DB_PASSWORD
+)
+
+mongoose
+  .connect(dbConnection)
+  .then(() => {
+    console.log('DB connection succesful')
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database')
+    throw err;
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server up and running at port ${PORT}`);
+});
